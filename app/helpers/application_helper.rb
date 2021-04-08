419f8a24abf1582a4dc5576baa9a541f7581bdc5
Diego Solis <disc3110@gmail.com>
2021-04-08 14:17:39 -0500

    move logic to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def logout_button
+    link_to 'Logout', destroy_user_session_path, method: :delete, class: "nav-link" if user_signed_in?
+  end
+  def edit_profile_button
+    link_to 'Edit Profile', edit_user_registration_path, class:"nav-link" if user_signed_in?
+  end
+  def sign_up_button
+    link_to 'Sign Up', new_user_registration_path, class:"nav-link" if !user_signed_in?
+  end
+  def sign_in_button
+    link_to 'Sign In', new_user_session_path, class:"nav-link" if !user_signed_in?
+  end
+  def disp_alerts
+    render 'layouts/alerts' if notice
+  end
+
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,15 @@
 module PostsHelper
+  def post_author(post)
+    "Posted by: #{post.user.username}" if user_signed_in?
+  end
+  def disp_form_errors
+    if @post.errors.any?
+      @post.errors.each do |error|
+        error.full_message
+      end
+    end
+  end
+  def create_posts
+    render 'form', post: @post if user_signed_in?
+  end
 end
