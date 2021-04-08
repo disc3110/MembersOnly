249ec68668e44f4cfe7f88179e008cd9eaca7cbb
Diego Solis <disc3110@gmail.com>
2021-04-08 14:27:49 -0500

    fix linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,24 @@
 module ApplicationHelper
   def logout_button
-    link_to 'Logout', destroy_user_session_path, method: :delete, class: "nav-link" if user_signed_in?
+    link_to 'Logout', destroy_user_session_path, method: :delete, class: 'nav-link' if user_signed_in?
   end
+
   def edit_profile_button
-    link_to 'Edit Profile', edit_user_registration_path, class:"nav-link" if user_signed_in?
+    link_to 'Edit Profile', edit_user_registration_path, class: 'nav-link' if user_signed_in?
   end
+
   def sign_up_button
-    link_to 'Sign Up', new_user_registration_path, class:"nav-link" if !user_signed_in?
+    link_to 'Sign Up', new_user_registration_path, class: 'nav-link' unless user_signed_in?
   end
+
   def sign_in_button
-    link_to 'Sign In', new_user_session_path, class:"nav-link" if !user_signed_in?
+    link_to 'Sign In', new_user_session_path, class: 'nav-link' unless user_signed_in?
   end
+
   def disp_alerts
     render 'layouts/alerts' if notice
   end
+
   def signed_in_only!
     redirect_to new_user_session_path unless signed_in?
   end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,15 +2,12 @@ module PostsHelper
   def post_author(post)
     "Posted by: #{post.user.username}" if user_signed_in?
   end
+
   def disp_form_errors
-    if @post.errors.any?
-      @post.errors.each do |error|
-        error.full_message
-      end
-    end
+    @post.errors.each(&:full_message) if @post.errors.any?
   end
+
   def create_posts
     render 'form', post: @post if user_signed_in?
   end
-
 end
