489b32208f67eb459c69ff2b06799ee945bb127f
Diego Solis <disc3110@gmail.com>
2021-04-08 14:58:16 -0500

    fix error while creating post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: %i[index show]
-  before_action :signed_in_only!, only: %i[new create]
 
   # GET /posts or /posts.json
   def index
